78eef9dd9cb4453ac4ee25ea66ce3fff0c90ddee
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-11-18 15:13:42 +0000

    Update axios.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -6,3 +6,16 @@ axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+ axiosRes.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401 || error.response?.status === 404) {
+        localStorage.removeItem("currentUser");
+        window.location.href = "/"; // Redirect to login page
+        }
+        return Promise.reject(error);
+    }
+    );
+  
+  
--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -11,12 +11,12 @@ export const useRedirect = (userAuthStatus) => {
         await axios.post("/dj-rest-auth/token/refresh/");
         // if user is logged in, the code below will run
         if (userAuthStatus === "loggedIn") {
-          history.push("/posts");
+          history.push("/");
         }
       } catch (err) {
         // if user is not logged in, the code below will run
         if (userAuthStatus === "loggedOut") {
-          history.push("/posts");
+          history.push("/");
         }
       }
     };
