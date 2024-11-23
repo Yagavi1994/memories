9990eb2942dcb3017c3d848d193cf8674f1fcf55
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-11-23 17:06:19 +0000

    Remove form container class from post create form.

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -119,7 +119,7 @@ function PostCreateForm() {
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`} id={appStyles.FormContainer} 
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`} 
           >
             <Form.Group className="text-center">
               {image ? (
